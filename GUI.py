b821ea62e93d4403c062f3ca4a96cbe6e774c425
ankitaich09 <31042284+ankitaich09@users.noreply.github.com>
2019-06-17 17:38:03 -0400

    Includes Scaling Images and Binning histograms
    Histogram Plotting is faster now, (from 30 seconds to 1-2 seconds)
    Histograms are automatically binned
    Option to scale the image to size of screen (automatically detects monitor size and works)

--- GUI.py
+++ GUI.py
@@ -5,33 +5,48 @@ import matplotlib.pyplot as plt
 from tkinter import filedialog
 from tkinter import *
 from tkinter import messagebox
+import time
+import fullimageshow as fis
 
 
 def openAndSavePic():
-    imageName = filedialog.askopenfilename()
-    _, exten = str(imageName).split('.')
-    if not(exten == "nef"):
-        messagebox.showerror("Wrong Extension", "Please choose a nef rawfile")
-    img = rawpy.imread(imageName)
-    rgbImage = img.postprocess()
-    plt.imshow(rgbImage)
+    rgb = callOpener()
+    plt.imshow(rgb)
 
 
-def rgb2gray(rgb):
-    r, g, b = rgb[:, :, 0], rgb[:, :, 1], rgb[:, :, 2]
+def scale():
+    w = window.winfo_screenheight()
+    h = window.winfo_screenwidth()
+    rgb = callOpener()
+    fis.imshow(rgb, h, w)
+
+
+def rgb2gray(rgbIm):
+    r, g, b = rgbIm[:, :, 0], rgbIm[:, :, 1], rgbIm[:, :, 2]
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
     return gray
 
 
 def hist():
+    rgb = callOpener()
+    gray = rgb2gray(rgb)
+    tstart = time.time()
+    plt.hist(gray, histtype= 'step')
+    tend = time.time()
+    taken = (tend-tstart)/60
+    messagebox.showinfo("Timer", message=taken)
+
+
+def callOpener():
     imageName = filedialog.askopenfilename()
-    img = rawpy.imread(imageName)
-    _, exten = str(imageName).split('.')
+    fname, exten = str(imageName).split('.')
     if not (exten == "nef"):
         messagebox.showerror("Wrong Extension", "Please choose a nef rawfile")
+    img = rawpy.imread(imageName)
     rgbImage = img.postprocess()
-    gray = rgb2gray(rgbImage)
-    plt.hist(gray)
+    return rgbImage
+
+
 
 
 window = Tk()
@@ -41,4 +56,7 @@ openButton = Button(window, text='Choose a pic', command=openAndSavePic)
 openButton.grid(column=2, row=2)
 histogramButton = Button(window, text='Plot a histogram', command=hist)
 histogramButton.grid(column=2, row=4)
+scaleButton = Button(window, text='scale an image to fullscreen', command=scale)
+scaleButton.grid(column=2, row=6)
 window.mainloop()
+
